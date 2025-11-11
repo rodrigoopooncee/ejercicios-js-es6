db41fe95155edcc5f17aef9788e286f10eb16088
rodrigoopooncee <rodrigoponvie@gmail.com>
2025-11-11 09:50:49 +0000

    Add files via upload

---
+++ 5.6.js
@@ -0,0 +1,30 @@
+/*Dado el siguiente javascript, utiliza .filter() para mostrar por consola 
+los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];*/
+
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+const input$$ = document.querySelector('[data-function="toFilterStreamers"]');
+
+
+const filtrar = (evento) => {
+    const texto = evento.target.value.toLowerCase();
+    const streamersFiltrados = streamers.filter((streamer) => {
+        const nombreEnMinusculas = streamer.name.toLowerCase();
+        return nombreEnMinusculas.includes(texto);
+    });
+    console.log(streamersFiltrados);
+};
+
+input$$.addEventListener('input', filtrar);
